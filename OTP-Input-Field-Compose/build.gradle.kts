b7cc3d32c76229c7aa4e85f7d180f6829b06d2c2
Paint <AHMED00AHMED00GAMAL@gmail.com>
2024-10-14 23:00:56 +0200

    change maven in gradle

--- OTP-Input-Field-Compose/build.gradle.kts
+++ OTP-Input-Field-Compose/build.gradle.kts
@@ -1,5 +1,3 @@
-import io.grpc.internal.SharedResourceHolder.release
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -33,6 +31,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose= true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion ="1.9.2"
+    }
 }
 
 dependencies {
@@ -49,4 +53,18 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+//            release(MavenPublication) {
+//                from components.release
+//
+//                groupId = 'com.ag.projects.otp_input_field_compose'
+//                artifactId = 'otp-compose'
+//                version = '1.0'
+//            }
+        }
+    }
 }
